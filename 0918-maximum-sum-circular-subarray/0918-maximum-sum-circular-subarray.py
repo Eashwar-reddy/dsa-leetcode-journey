032b3aa4980d0b11b9271da9a86c4cabdce768c2
Eashwar-reddy <158137174+Eashwar-reddy@users.noreply.github.com>
2026-05-01 16:30:27 +0530

    Time: 107 ms (46.52%), Space: 14.8 MB (92.16%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def maxSubarraySumCircular(self, nums):
-        currmax=globalmax=nums[0]
-        currmin=globalmin=nums[0]
         total=nums[0]
+        currmin=globalmin=nums[0]
+        currmax=globalmax=nums[0]
         for i in nums[1:]:
-            currmax=max(currmax+i,i)
-            globalmax=max(globalmax,currmax)
             currmin=min(currmin+i,i)
-            globalmin=min(currmin,globalmin)
+            globalmin=min(globalmin,currmin)
+            currmax=max(currmax+i,i)
+            globalmax=max(currmax,globalmax)
             total+=i
         if(globalmax<0):
             return globalmax
-        return(max(globalmax,total-globalmin))
+        return max(globalmax,total-globalmin)
